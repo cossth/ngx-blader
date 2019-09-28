4acdcb998b557b1712dc6e0243a7c3eb2c194d3e
Shubham Sharma <sth@live.in>
2019-09-28 19:31:46 +0530

    WTF Commit

---
+++ libs/ngx-blader/src/lib/components/blade/blade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bd-blade',
+  templateUrl: './blade.component.html',
+  styleUrls: ['./blade.component.css']
+})
+export class BladeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
